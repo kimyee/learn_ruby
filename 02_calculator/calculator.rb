451f40a335392da107c4616f771bcd0e56ed2e2f
kimyee <kimyee.16@gmail.com>
2013-08-26 19:31:59 -0400

    calculator

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,28 @@
+def add(x,y)
+	x + y
+end
+
+def subtract(x,y)
+	x - y
+end
+
+def sum(x)
+	x.inject(0, :+)
+end
+
+def multiply(x)
+	x.inject(:*)
+end
+
+def factorial(x)
+	if x == 0
+		return 0
+	end
+	if x == 1
+		return 1
+	end
+	factorial(x-1)*x
+		
+		
+end
+
