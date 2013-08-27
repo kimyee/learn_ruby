4267c420136806c91c2750e1a6fe5e9791e69d7d
kimyee <kimyee.16@gmail.com>
2013-08-27 13:36:08 -0400

    complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -9,3 +9,17 @@ end
 def repeat(hello, h=2)
 	h.times.collect{hello}.join(' ')
 end
+
+def start_of_word(hello, num)
+	hello[0, num]
+end
+
+def first_word(string)
+	string.split[0]
+end
+
+def titleize(string)
+	lowercase_words = %w{and the over}
+	string.split.each_with_index.map{|x, index| lowercase_words.include?(x) && index > 0 ? x : x.capitalize}.join(" ")
+end
+
